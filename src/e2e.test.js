1829e6f4de2108aa42202124ef4848b839797acb
sneha0353 <snehas.18.becs@acharya.ac.in>
2022-05-21 19:53:13 +0530

    test

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -18,14 +18,14 @@ describe("App.js", () => {
     const text = await page.$eval("#form-test", (e) => e.textContent);
     expect(text).toContain("FORM VALIDATION");
 
-    // await page.select('#validationCustom04', 'Leanne Graham');
-    // await page.waitForSelector('#inputAddress222');
-    // await page.$eval('#inputAddress222', el => el.value = 'abcd');
-    // await page.waitForSelector('#inputAddress333');
-    // await page.$eval('#inputAddress333', el => el.value = 'defg');
-    // await page.click('#submitButton');
-    // const text2 = await page.waitForSelector('#submitted', (e) => e.textContent);
-    // expect(text2).toContain("Success")
+    await page.select('#validationCustom04', 'Leanne Graham');
+    await page.waitForSelector('#inputAddress222');
+    await page.$eval('#inputAddress222', el => el.value = 'abcd');
+    await page.waitForSelector('#inputAddress333');
+    await page.$eval('#inputAddress333', el => el.value = 'defg');
+    await page.click('#submitButton');
+    const text2 = await page.waitForSelector('#submitted', (e) => e.textContent);
+    expect(text2).toContain("Success")
   });
   },30000);
   
