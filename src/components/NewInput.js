d86aa398400fc43819378fa29e255461a48ce6fc
sneha0353 <snehas.18.becs@acharya.ac.in>
2022-05-24 10:44:35 +0530

    reference comments

--- src/components/NewInput.js
+++ src/components/NewInput.js
@@ -1,4 +1,4 @@
-import React, { useState,useCallback } from "react";
+import React, { useState} from "react";
 import axios from "axios";
 import Geo from "./geo";
 import { ToastContainer, toast } from "react-toastify";
@@ -6,9 +6,11 @@ import "react-toastify/dist/ReactToastify.css";
 
 function NewInput(props) {
   const [title, setTitle] = useState("fcgh");
-  const [body, setBody] = useState();
+  const [body, setBody] = useState("fcgh");
   const [loading, setLoading] = useState(false);
   const [flag, setFlag] = useState(false);
+  //const titleRef=useRef(null)
+  //const bodyRef=useRef(null)
 
   const handleSubmit = () => {
     setLoading(true);
@@ -23,7 +25,7 @@ function NewInput(props) {
               id: id,
               userId: props.allVal.id,
               title: title,
-              body: body
+              body: body,
             })
             .then((data) => {
               setFlag(true);
@@ -43,7 +45,7 @@ function NewInput(props) {
   const Loader = () => {
     return (
       <div className="spinner-border text-light" role="status">
-        <span className="sr-only">{" "}</span>
+        <span className="sr-only"> </span>
       </div>
     );
   };
@@ -51,201 +53,203 @@ function NewInput(props) {
   return (
     <div>
       <div className="row g-3 mt-1">
-      <ToastContainer 
-      id="hello"
-      />
-      <h5 id="suc">successful</h5>
-      <div className="col-12">
-        <label htmlFor="inputAddress" className="form-label">
-          Name :
+        <ToastContainer id="hello" />
+        <h5 id="suc">successful</h5>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">
+            Name :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="Name"
+            value={props.allVal ? props.allVal.name : ""}
+          />
+        </div>
+        <div className="col-6">
+          <label htmlFor="inputAddress" className="form-label">
+            Id :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Id"
+            value={props.allVal ? props.allVal.id : ""}
+          />
+        </div>
+        <div className="col-6">
+          <label htmlFor="inputAddress" className="form-label">
+            Username :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            value={props.allVal ? props.allVal.username : ""}
+          />
+        </div>
+        <label for="inputAddress" className="form-label mb-1">
+          Address
         </label>
-        <input
-          type="text"
-          className="form-control"
-          id="inputAddress"
-          placeholder="Name"
-          value={props.allVal ? props.allVal.name : ""}
-        />
-      </div>
-      <div className="col-6">
-        <label htmlFor="inputAddress" className="form-label">
-          Id :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Id"
-          value={props.allVal ? props.allVal.id : ""}
-        />
-      </div>
-      <div className="col-6">
-        <label htmlFor="inputAddress" className="form-label">
-          Username :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Username"
-          value={props.allVal ? props.allVal.username : ""}
-        />
-      </div>
-      <label for="inputAddress" className="form-label mb-1">
-        Address
-      </label>
-      <div className="col-6">
-        <label for="inputAddress" className="form-label">
-          Street :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Street"
-          value={props.allVal ? props.allVal.address.street : ""}
-        />
-      </div>
-      <div class="col-6">
-        <label for="inputAddress" className="form-label">
-          Suite :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Suite"
-          value={props.allVal ? props.allVal.address.suite : ""}
-        />
-      </div>
-      <div className="col-6 mt-1">
-        <label for="inputAddress" className="form-label">
-          City :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="City"
-          value={props.allVal ? props.allVal.address.city : ""}
-        />
-      </div>
-      <div className="col-6 mt-1">
-        <label for="inputAddress" className="form-label">
-          Zipcode :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Zipcode"
-          value={props.allVal ? props.allVal.address.zipcode : ""}
-        />
-      </div>
+        <div className="col-6">
+          <label for="inputAddress" className="form-label">
+            Street :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Street"
+            value={props.allVal ? props.allVal.address.street : ""}
+          />
+        </div>
+        <div class="col-6">
+          <label for="inputAddress" className="form-label">
+            Suite :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Suite"
+            value={props.allVal ? props.allVal.address.suite : ""}
+          />
+        </div>
+        <div className="col-6 mt-1">
+          <label for="inputAddress" className="form-label">
+            City :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="City"
+            value={props.allVal ? props.allVal.address.city : ""}
+          />
+        </div>
+        <div className="col-6 mt-1">
+          <label for="inputAddress" className="form-label">
+            Zipcode :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Zipcode"
+            value={props.allVal ? props.allVal.address.zipcode : ""}
+          />
+        </div>
 
-      <label for="inputAddress" className="form-label mt-4 mb-1">
-        Contact Details
-      </label>
-      <div className="col-6">
-        <label for="inputAddress" className="form-label">
-          Phone :
+        <label for="inputAddress" className="form-label mt-4 mb-1">
+          Contact Details
         </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Phone"
-          value={props.allVal ? props.allVal.phone : ""}
-        />
-      </div>
-      <div className="col-6">
-        <label for="inputAddress" className="form-label">
-          Website :
+        <div className="col-6">
+          <label for="inputAddress" className="form-label">
+            Phone :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Phone"
+            value={props.allVal ? props.allVal.phone : ""}
+          />
+        </div>
+        <div className="col-6">
+          <label for="inputAddress" className="form-label">
+            Website :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Website"
+            value={props.allVal ? props.allVal.website : ""}
+          />
+        </div>
+        <label for="inputAddress" className="form-label mb-1">
+          Company
         </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Website"
-          value={props.allVal ? props.allVal.website : ""}
-        />
+        <div className="col-12">
+          <label for="inputAddress" className="form-label">
+            Name :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            value={props.allVal ? props.allVal.company.name : ""}
+          />
+        </div>
+        <div className="col-12">
+          <label for="inputAddress" className="form-label">
+            CatchPhrase :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Catch Phrase"
+            value={props.allVal ? props.allVal.company.catchPhrase : ""}
+          />
+        </div>
+        <div className="col-12">
+          <label for="inputAddress" className="form-label">
+            Bs :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="BS"
+            value={props.allVal ? props.allVal.company.bs : ""}
+          />
+        </div>
+        <div className="col-12">
+          <Geo value={props.allVal ? props.allVal : ""} />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">
+            Title :
+          </label>
+          <input
+            //ref={titleRef}
+            type="text"
+            className="form-control"
+            id="inputAddress222"
+            placeholder="Title"
+            onChange={(e)=>setTitle(e.target.value)}
+            // onChange={onChangeInput}
+            // onInput={(e) => {
+            //   console.log(e.target.value);
+            //   setTitle(e.target.value);
+            // }}
+          />
+          {title ? <div></div> : <div>Please enter title.</div>}
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">
+            Body :
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress333"
+            placeholder="Body"
+            //ref={bodyRef}
+            onChange={(e) => setBody(e.target.value)}
+          />
+          {body ? <div></div> : <div>Please enter body.</div>}
+        </div>
+        {title && body ? (
+          <button
+            type="button"
+            id="submitButton"
+            onClick={handleSubmit}
+            className="btn btn-lg btn-secondary"
+          >
+            {loading ? <Loader /> : "Submit"}
+          </button>
+        ) : (
+          <button type="button" className="btn btn-lg btn-secondary" disabled>
+            Submit
+          </button>
+        )}
+        {flag && <div id="submitted">Success</div>}
       </div>
-      <label for="inputAddress" className="form-label mb-1">
-        Company
-      </label>
-      <div className="col-12">
-        <label for="inputAddress" className="form-label">
-          Name :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Name"
-          value={props.allVal ? props.allVal.company.name : ""}
-        />
-      </div>
-      <div className="col-12">
-        <label for="inputAddress" className="form-label">
-          CatchPhrase :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Catch Phrase"
-          value={props.allVal ? props.allVal.company.catchPhrase : ""}
-        />
-      </div>
-      <div className="col-12">
-        <label for="inputAddress" className="form-label">
-          Bs :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="BS"
-          value={props.allVal ? props.allVal.company.bs : ""}
-        />
-      </div>
-      <div className="col-12">
-        <Geo value={props.allVal ? props.allVal : ""} />
-      </div>
-      <div className="col-12">
-        <label htmlFor="inputAddress" className="form-label">
-          Title :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="inputAddress222"
-          placeholder="Title"
-         // onChange={onChangeInput}
-          onInput={(e)=>{
-            console.log(e.target.value)
-            setTitle(e.target.value)}}
-        />
-        { title ? <div></div> : <div>Please enter title.</div>}
-      </div>
-      <div className="col-12">
-        <label htmlFor="inputAddress" className="form-label">
-          Body :
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="inputAddress333"
-          placeholder="Body"
-          onChange={(e)=>setBody(e.target.value)}
-        />
-        {!body ? <div>Please enter body.</div> : <div></div>}
-      </div>
-      {title && body ? (
-        <button
-          type="button"
-          id="submitButton"
-          onClick={handleSubmit}
-          className="btn btn-lg btn-secondary"
-        >
-          {loading ? <Loader /> : "Submit"}
-        </button>
-      ) : (
-        <button type="button" className="btn btn-lg btn-secondary" disabled>
-          Submit
-        </button>
-      )}
-      {flag && <div id="submitted">Success</div>}
-    </div>
     </div>
   );
 }
